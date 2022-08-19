b44c42e7ead0ffa413172730f912175122621fe5
agarza112 <alga2790@student.harmonytx.org>
2022-08-19 08:54:48 -0500

    This is my first commit

---
+++ MyFirstClass.java
@@ -0,0 +1,24 @@
+public class MyFirstClass {
+
+    // after you creature your project and class
+    // you have to create your main method to run commands
+    // In order to uaw shortcut you type main and pre <tB>
+    public static void main(String[] args)
+    {
+//          to print out something to the screen you type System.out.println("your text here");
+//          each line of code must end ith semicolon. ;
+//          shortcut or System.out.println() is sout and <tab>
+//          this double // mean this is not part of your code it is just a comment line to explain or taking note
+
+//          this is single line comment but if you need multiple line
+
+
+//          this is multiple line comment until the end of the comment you can type your comment
+
+        System.out.println("My first line of code");
+
+        // another way to print on screen im h next line
+        // print pints the text in the same line until you change it
+
+    }
+}
